41b3e89267a747ee8a4f2394d8de471daa7fcc12
Christina Robinson <christinaro@christinaro-macbookpro.roam.corp.google.com>
2017-07-31 21:24:29 -0700

    First commit

---
+++ app/src/main/java/com/example/christinaro/lab4/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.christinaro.lab4;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment fragment = new ExchangeRateListFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+
+    }
+
+
+}
